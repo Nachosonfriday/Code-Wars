caae3e509750d73ee6c904b32fc0b437c4e4d806
Manuel Zuniga <zuniga@ualberta.ca>
2022-04-19 20:01:52 -0600

    added alternate solution

--- createPhoneNumbers.js
+++ createPhoneNumbers.js
@@ -4,3 +4,15 @@ function createPhoneNumber(numbers){
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+//alternate solution
+// function createPhoneNumber(numbers){
+//   var format = "(xxx) xxx-xxxx";
+  
+//   for(var i = 0; i < numbers.length; i++)
+//   {
+//     format = format.replace('x', numbers[i]);
+//   }
+  
+//   return format;
+// }
